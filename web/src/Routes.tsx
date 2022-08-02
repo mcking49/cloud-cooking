489f2a4ef559b509871ea59a6d97131059b0ba6e
Miten Chauhan <miten@chauhan.nz>
2022-08-02 20:36:32 +1200

    Delete temp pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,9 +12,6 @@ const Routes = () => {
       <Set wrap={AuthLayout}>
         <Route path="/login" page={LoginPage} name="login" />
         <Route path="/signup" page={SignupPage} name="signup" />
-        <Route path="/temp-login" page={TempLoginPage} name="tempLogin" />
-        <Route path="/temp-forgot-password" page={TempForgotPasswordPage} name="tempForgotPassword" />
-        <Route path="/temp-reset-password" page={TempResetPasswordPage} name="tempResetPassword" />
       </Set>
 
       <Route path="/" page={HomePage} name="home" />
