3a2fd84c78ea72525debf62ae8b8da195cd45a3f
Miten Chauhan <miten@chauhan.nz>
2022-08-08 08:17:01 +1200

    Update theme

---
+++ web/src/theme/components/Link.ts
@@ -0,0 +1,34 @@
+import { ComponentStyleConfig, CSSObject } from '@chakra-ui/react'
+
+const sideMenuNormal: CSSObject = {
+  color: 'blue.800',
+  display: 'inline-flex',
+  alignItems: 'center',
+  fontFamily: 'PT Sans',
+  fontSize: '16px',
+  fontWeight: 400,
+  height: '52px',
+  letterSpacing: '0.04em',
+  lineHeight: '1.2em',
+  padding: '16px 44px',
+  textTransform: 'uppercase',
+  width: 'full',
+}
+
+const sideMenuActive: CSSObject = {
+  ...sideMenuNormal,
+  background: 'white',
+  fontWeight: 700,
+  letterSpacing: '0.08em',
+  shadow: 'sideNav',
+  width: 'calc(100% + 4px)',
+}
+
+const Link: ComponentStyleConfig = {
+  variants: {
+    sideMenuActive,
+    sideMenuNormal,
+  },
+}
+
+export default Link
