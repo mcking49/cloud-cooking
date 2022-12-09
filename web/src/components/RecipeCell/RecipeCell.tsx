f8ad8b88108a7287e5809a387fbfe9312c8f21f9
Miten Chauhan <miten@chauhan.nz>
2022-12-09 17:17:19 +1300

    Implement recipe ingredients

--- web/src/components/RecipeCell/RecipeCell.tsx
+++ web/src/components/RecipeCell/RecipeCell.tsx
@@ -1,13 +1,25 @@
-import { Heading, HStack, Icon, Tag, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
+
+import {
+  Heading,
+  HStack,
+  Icon,
+  ListItem,
+  Tag,
+  UnorderedList,
+  VStack,
+} from '@chakra-ui/react'
 import { capitalize } from 'lodash/fp'
 import { AiFillClockCircle } from 'react-icons/ai'
 import { FaUser } from 'react-icons/fa'
 import { GiForkKnifeSpoon } from 'react-icons/gi'
 import type { FindRecipeQuery, FindRecipeQueryVariables } from 'types/graphql'
 
-import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import type { CellFailureProps, CellSuccessProps } from '@redwoodjs/web'
 
 import { SideMenuHeader } from 'src/layouts/SideMenuLayout'
+
+type RecipeTab = 'INGREDIENTS' | 'DIRECTIONS'
 
 export const QUERY = gql`
   query FindRecipeQuery($id: Int!) {
@@ -51,6 +63,8 @@ export const Success = ({
   recipe,
 }: CellSuccessProps<FindRecipeQuery, FindRecipeQueryVariables>) => {
   console.log('cell success', recipe)
+  // TODO: use _setActiveTab
+  const [activeTab, _setActiveTab] = useState<RecipeTab>('INGREDIENTS')
 
   return (
     <>
@@ -121,6 +135,65 @@ export const Success = ({
           </HStack>
         </VStack>
       </SideMenuHeader>
+
+      <VStack alignItems="flex-start" w="full" minHeight="full" spacing={6}>
+        <HStack justifyContent="space-between" w="full">
+          <Heading as="h1" fontWeight={400} fontSize="16px" lineHeight="1.25em">
+            Ingredients
+          </Heading>
+
+          <div>buttons</div>
+        </HStack>
+
+        {activeTab === 'INGREDIENTS' ? (
+          <VStack h="full" w="full" spacing={4}>
+            {recipe.ingredientGroups.map((ingredientGroup) => (
+              <VStack
+                key={ingredientGroup.id}
+                w="full"
+                background="white"
+                borderRadius="6px"
+                paddingY={3}
+                paddingX={4}
+                alignItems="flex-start"
+                spacing={3}
+              >
+                <Heading
+                  as="h2"
+                  color="green.400"
+                  fontFamily="Open Sans"
+                  fontWeight={600}
+                  fontSize="10px"
+                  lineHeight="1.4em"
+                  letterSpacing="0.08em"
+                  textTransform="uppercase"
+                >
+                  {ingredientGroup.name}
+                </Heading>
+
+                {ingredientGroup.ingredients.length && (
+                  <UnorderedList>
+                    {ingredientGroup.ingredients.map((ingredient, index) => (
+                      <ListItem
+                        key={`${index}_${ingredient}`}
+                        fontFamily=" PT Sans"
+                        fontWeight={400}
+                        lineHeight="1.25em"
+                        fontSize="16px"
+                        ml={6}
+                      >
+                        {ingredient}
+                      </ListItem>
+                    ))}
+                  </UnorderedList>
+                )}
+              </VStack>
+            ))}
+          </VStack>
+        ) : (
+          <div>directions tab</div>
+        )}
+      </VStack>
       <div>{JSON.stringify(recipe)}</div>
     </>
   )
