c6b5f407dc9d65a383f15898650aea9d21916a15
Miten Chauhan <miten@chauhan.nz>
2022-12-09 16:56:58 +1300

    Style header for recipe page

--- web/src/components/RecipeCell/RecipeCell.tsx
+++ web/src/components/RecipeCell/RecipeCell.tsx
@@ -1,6 +1,13 @@
+import { Heading, HStack, Icon, Tag, VStack } from '@chakra-ui/react'
+import { capitalize } from 'lodash/fp'
+import { AiFillClockCircle } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { GiForkKnifeSpoon } from 'react-icons/gi'
 import type { FindRecipeQuery, FindRecipeQueryVariables } from 'types/graphql'
 
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+import { SideMenuHeader } from 'src/layouts/SideMenuLayout'
 
 export const QUERY = gql`
   query FindRecipeQuery($id: Int!) {
@@ -45,5 +52,76 @@ export const Success = ({
 }: CellSuccessProps<FindRecipeQuery, FindRecipeQueryVariables>) => {
   console.log('cell success', recipe)
 
-  return <div>{JSON.stringify(recipe)}</div>
+  return (
+    <>
+      <SideMenuHeader>
+        <VStack
+          justifyContent="flex-end"
+          w="full"
+          h="full"
+          alignItems="flex-start"
+          spacing={2}
+        >
+          <Heading as="h1" fontWeight={400} lineHeight="1.2em" mt={4}>
+            {recipe.name}
+          </Heading>
+
+          <HStack spacing={2}>
+            {recipe.categories.map((category) => (
+              <Tag
+                key={category}
+                variant="solid"
+                backgroundColor="#F3D191"
+                color="blue.800"
+                borderRadius="4px"
+                fontSize="8px"
+                lineHeight="1.2em"
+                padding="6px 8px"
+                minHeight={0}
+                minWidth={0}
+              >
+                <Icon as={GiForkKnifeSpoon} mr="6px" />
+                {capitalize(category)}
+              </Tag>
+            ))}
+
+            {recipe.length && (
+              <Tag
+                variant="solid"
+                backgroundColor="#F3D191"
+                color="blue.800"
+                borderRadius="4px"
+                fontSize="8px"
+                lineHeight="1.2em"
+                padding="6px 8px"
+                minHeight={0}
+                minWidth={0}
+              >
+                <Icon as={AiFillClockCircle} mr="6px" />
+                {recipe.length} mins
+              </Tag>
+            )}
+
+            {recipe.servings && (
+              <Tag
+                variant="solid"
+                backgroundColor="#F3D191"
+                color="blue.800"
+                borderRadius="4px"
+                fontSize="8px"
+                lineHeight="1.2em"
+                padding="6px 8px"
+                minHeight={0}
+                minWidth={0}
+              >
+                <Icon as={FaUser} mr="6px" />
+                {recipe.servings} people
+              </Tag>
+            )}
+          </HStack>
+        </VStack>
+      </SideMenuHeader>
+      <div>{JSON.stringify(recipe)}</div>
+    </>
+  )
 }
